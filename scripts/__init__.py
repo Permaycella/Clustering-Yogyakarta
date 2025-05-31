e8ccb314b55f495f5cfb0645f5ef416ff9e3498e
Permaycella <permaycellaananta@gmail.com>
2025-05-31 14:19:07 +0700

    Add files via upload

---
+++ scripts/__init__.py
@@ -0,0 +1,6 @@
+from . import clustering_bantul
+from . import clustering_gunung_kidul
+from . import clustering_kota_yogyakarta
+from . import clustering_kulon_progo
+from . import clustering_sleman
+from . import lat_long
